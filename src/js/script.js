525113c629dc19dabd296076a1ba22a524ec434f
Michal Plewa <97453813+MichPlewa@users.noreply.github.com>
2022-03-02 19:57:54 +0100

    price calculation added

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,11 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderFrom();
+
+      
 
       console.log('new product: ', thisProduct);
     }
@@ -77,28 +81,101 @@
 
       menuContainer.appendChild(thisProduct.element);
     }
+
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion(){
       const thisProduct = this;
 
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
-      clickableTrigger.addEventListener('click', function(event) {
-      
+      //clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event){
         event.preventDefault();
 
         const activeProduct = document.querySelector(select.all.menuProductsActive);
-        console.log('activeProduct', activeProduct);
-        console.log(thisProduct.element);
+        //console.log('activeProduct', activeProduct);
+        //console.log(thisProduct.element);
 
         if(activeProduct && activeProduct != thisProduct.element){
 
           activeProduct.classList.remove('active');
-          console.log('if');
+          //console.log('if');
 
         }
         thisProduct.element.classList.toggle('active');
         
       });
+    }
+
+    initOrderFrom(){
+      const thisProduct = this;
+      console.log('==initOrderFrom==');
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log('param: ', paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+        
+          //console.log('formData:  ', formData);
+
+          if(formData[paramId] && formData[paramId].includes(optionId)){
+            if(option.default != true){
+              price = price + option.price;
+            }
+            else {
+              if (option.default == true){
+                price = price - option.price;
+              }
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
@@ -111,6 +188,7 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     },
+
     initData: function () {
       const thisApp = this;
 
